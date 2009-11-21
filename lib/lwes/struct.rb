176ca54dea4a0b48ad58b368095ce6f2d9c0b15b
Erik S. Chang <erik.s.chang@gmail.com>
2009-11-21 04:14:51 +0000

    overhaul struct initializers, options hash only and a lot of options now supported
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-ruby/trunk@310 a2f82657-cdd2-4550-bd36-68a8e7111808

--- lib/lwes/struct.rb
+++ lib/lwes/struct.rb
@@ -1,16 +1,51 @@
 module LWES
   class Struct
 
-    # creates a new Struct-based class from the given ESF +file+
-    # with the given +klass_sym+ as the name.  Options may
-    # include :event_class (in case it differs in the ESF file)
-    # and the :parent_class (Object).
-    def self.new(file, klass_sym, options = {}, &block)
+    # creates a new Struct-based class, takes the following
+    # options hash:
+    #
+    #   :file     - pathname to the ESF file, this is always required
+    #   :class    - Ruby base class name, if the ESF file only has one
+    #               event defined (besides MetaEventInfo), then specifying
+    #               it is optional, otherwise it is required when multiple
+    #               events are defined in the same ESF :file given above
+    #   :parent   - parent class or module, the default is 'Object' putting
+    #               the new class in the global namespace.
+    #   :event    - event name if it differs from the Ruby base class name
+    #               given (or inferred) above.  For DRY-ness, you are
+    #               recommended to keep your event names and Ruby class
+    #               names in sync and not need this option.
+    #   :optional - Array of field names that are optional, the special
+    #               "MetaEventInfo" name makes all elements defined in
+    #               the MetaEventInfo section of the ESF is optional.
+    #               This may also be a regular expression.
+    #   :skip     - Array of field names to skip from the Event defininition
+    #               entirely, these could include fields that are only
+    #               implemented by the Listener.  This may also be a
+    #               regular expression.
+    #   :defaults - hash of default key -> value pairs to set at
+    #               creation time
+    #
+    def self.new(options, &block)
+      file = options[:file] or raise ArgumentError, "esf file missing"
+      test ?r, file or raise ArgumentError, "file #{file.inspect} not readable"
       dump = TypeDB.new(file).to_hash
-      event_class = options[:event_class] || klass_sym
-      parent_class = options[:parent_class] || Object
-      event_def = dump[event_class] or
-        raise RuntimeError, "#{event_class.inspect} not defined in #{file}"
+      klass = options[:class] || begin
+        # make it easier to deal with single event files
+        events = (dump.keys -  [ :MetaEventInfo ])
+        events.size > 1 and
+            raise RuntimeError,
+                  "multiple event defs available: #{events.inspect}\n" \
+                  "pick one with :class"
+        events.first
+      end
+
+      name = options[:name] || klass
+      parent = options[:parent] || Object
+      event_def = dump[name.to_sym] or
+        raise RuntimeError, "#{name.inspect} not defined in #{file}"
+
+      # merge MetaEventInfo fields in
       meta_event_info = dump[:MetaEventInfo]
       alpha = proc { |a,b| a.first.to_s <=> b.first.to_s }
       event_def = event_def.sort(&alpha)
@@ -20,13 +55,81 @@ module LWES
           seen.include?(field_type.first) or event_def << field_type
         end
       end
-      klass = ::Struct.new(*(event_def.map { |(field,_)| field }))
-      klass = parent_class.const_set(klass_sym, klass)
-      type_db = klass.const_set :TYPE_DB, {}
+
+      Array(options[:skip]).each do |x|
+        if Regexp === x
+          event_def.delete_if { |(f,_)| x =~ f.to_s }
+        else
+          if x.to_sym == :MetaEventInfo
+            meta_event_info.nil? and
+              raise RuntimeError, "MetaEventInfo not defined in #{file}"
+            meta_event_info.each do |(field,_)|
+              event_def.delete_if { |(f,_)| field == f }
+            end
+          else
+            event_def.delete_if { |(f,_)| f == x.to_sym }
+          end
+        end
+      end
+
+      tmp = ::Struct.new(*(event_def.map { |(field,_)| field }))
+      tmp = parent.const_set(klass, tmp)
+      type_db = tmp.const_set :TYPE_DB, {}
       event_def.each { |(field,type)| type_db[field] = type }
-      klass.const_set :TYPE_LIST, event_def
-      klass.class_eval(&block) if block_given?
-      klass
+      tmp.const_set :TYPE_LIST, event_def
+
+      optional = tmp.const_set :OPTIONAL, {}
+      Array(options[:optional]).each do |x|
+        if Regexp === x
+          event_def.each { |(f,_)| optional[f] = true if x =~ f.to_s }
+        else
+          if x.to_sym == :MetaEventInfo
+            meta_event_info.nil? and
+              raise RuntimeError, "MetaEventInfo not defined in #{file}"
+            meta_event_info.each{ |(field,_)| optional[field] = true }
+          else
+            optional[x.to_sym] = true
+          end
+        end
+      end
+      defaults = options[:defaults] || {}
+      defaults = tmp.const_set :DEFAULTS, defaults.dup
+
+      # define a parent-level method, so instead of Event.new
+      # you could do
+      if false
+        (parent == Object ?
+          Kernel : parent.instance_eval { class << self; self; end }
+        ).__send__(:define_method, klass) do |*args|
+          if Hash === (init = args.first)
+            rv = tmp.new()
+            defaults.merge(init).each_pair { |k,v| rv[k] = v }
+            rv
+          else
+            rv = tmp.new(*args)
+            defaults.each_pair { |k,v| rv[k] ||= v }
+            rv
+          end
+        end
+      else
+        tmp.instance_eval { class << self; self; end }.instance_eval do
+          alias_method :_new, :new
+          define_method(:new) do |*args|
+            if Hash === (init = args.first)
+              rv = tmp._new()
+              defaults.merge(init).each_pair { |k,v| rv[k] = v }
+              rv
+            else
+              rv = tmp._new(*args)
+              defaults.each_pair { |k,v| rv[k] ||= v }
+              rv
+            end
+          end
+        end
+      end
+
+      tmp.class_eval(&block) if block_given?
+      tmp
     end
   end
 end
--- test/unit/test_struct.rb
+++ test/unit/test_struct.rb
@@ -1,5 +1,7 @@
 require "#{File.dirname(__FILE__)}/../test_helper"
 
+module Dummy
+end
 class TestStruct < Test::Unit::TestCase
 
    EXPECT_DB = {
@@ -20,6 +22,26 @@ class TestStruct < Test::Unit::TestCase
     :t_uint16 => LWES::U_INT_16,
     :t_int16 => LWES::INT_16
   }
+  EXPECT_LIST = [
+    # class-specific fields first
+    [ :t_bool, LWES::BOOLEAN],
+    [ :t_int16, LWES::INT_16 ],
+    [ :t_int32, LWES::INT_32],
+    [ :t_int64, LWES::INT_64],
+    [ :t_ip_addr, LWES::IP_ADDR],
+    [ :t_string, LWES::STRING],
+    [ :t_uint16, LWES::U_INT_16],
+    [ :t_uint32, LWES::U_INT_32],
+    [ :t_uint64, LWES::U_INT_64],
+
+    # MetaEventInfo
+    [ :ReceiptTime, LWES::INT_64],
+    [ :SenderIP, LWES::IP_ADDR],
+    [ :SenderPort, LWES::U_INT_16],
+    [ :SiteID, LWES::U_INT_16],
+    [ :enc, LWES::INT_16],
+    [ :st, LWES::STRING],
+  ]
 
   ESF_FILE = "#{File.dirname(__FILE__)}/test1.esf"
 
@@ -27,19 +49,20 @@ class TestStruct < Test::Unit::TestCase
     assert_instance_of Module, LWES, "LWES is not a module"
   end
 
-  def test_new
-    a = LWES::Struct.new(ESF_FILE, :Event1)
+  def test_new_with_defaults
+    a = LWES::Struct.new(:file=>ESF_FILE)
     assert a.instance_of?(Class)
     assert_equal "Event1", a.name
     assert a.const_get(:TYPE_DB).instance_of?(Hash)
     assert_equal EXPECT_DB, a.const_get(:TYPE_DB)
+    assert_equal EXPECT_LIST, a.const_get(:TYPE_LIST)
     y = a.new
     assert_equal "Event1", y.class.name
     assert_kind_of(::Struct, y)
   end
 
   def test_new_with_alt_name
-    a = LWES::Struct.new(ESF_FILE, :Event, :event_class => :Event1)
+    a = LWES::Struct.new(:file=>ESF_FILE,:class=>:Event,:name=>:Event1)
     assert a.instance_of?(Class)
     assert_equal "Event", a.name
     assert a.const_get(:TYPE_DB).instance_of?(Hash)
@@ -50,7 +73,7 @@ class TestStruct < Test::Unit::TestCase
   end
 
   def test_new_in_module
-    a = LWES::Struct.new(ESF_FILE, :Event1, :parent_class => LWES)
+    a = LWES::Struct.new(:file=>ESF_FILE,:class=>:Event1,:parent=>LWES)
     assert a.instance_of?(Class)
     assert_equal "LWES::Event1", a.name
     assert a.const_get(:TYPE_DB).instance_of?(Hash)
@@ -61,7 +84,7 @@ class TestStruct < Test::Unit::TestCase
   end
 
   def test_eval
-    a = LWES::Struct.new(ESF_FILE, :Eval, :event_class => :Event1) do
+    a = LWES::Struct.new(:file=>ESF_FILE,:class=>:Eval,:name=>:Event1) do
       def aaaaaaaa
         true
       end
@@ -77,6 +100,117 @@ class TestStruct < Test::Unit::TestCase
     assert_instance_of TrueClass, y.aaaaaaaa
     assert_equal a.const_get(:TYPE_DB).object_id, a.get_type_db.object_id
     assert_equal EXPECT_DB, a.get_type_db
+  end
+
+  def test_optional
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=>:Optional,
+                         :name=>:Event1,
+                         :optional => %w(SenderPort SiteID ReceiptTime))
+    expect_optional = {
+      :SenderPort => true,
+      :SiteID => true,
+      :ReceiptTime => true
+    }
+    assert_equal expect_optional, a.const_get(:OPTIONAL)
+  end
+
+  def test_optional_meta_event_info
+    expect = {
+      :ReceiptTime => true,
+      :SenderIP => true,
+      :SenderPort => true,
+      :SiteID => true,
+      :enc => true,
+      :st => true
+    }
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=>:MetaOptional,
+                         :name=>:Event1,
+                         :optional => %w(MetaEventInfo))
+    assert_equal expect, a.const_get(:OPTIONAL)
+  end
+
+
+  def test_new_initialize
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=>:Init,
+                         :name=>:Event1,
+                         :parent=>Dummy)
+    y = Dummy::Init.new(:t_ip_addr => "192.168.0.1", :t_bool => true)
+    assert_equal "192.168.0.1", y.t_ip_addr
+    assert_equal true, y.t_bool
+  end
+
+  def test_new_initialize_defaults
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=> :LWES_Blah,
+                         :name=>:Event1,
+                         :defaults => { :enc => 1 })
+    y = LWES_Blah.new
+    assert_equal 1, y.enc
+  end
+
+  def test_new_initialize_defaults_and_hash
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=> :LWES_Foo,
+                         :name=>:Event1,
+                         :defaults => { :enc => 1 })
+    y = LWES_Foo.new(:enc => 2)
+    assert_equal 2, y.enc
+  end
+
+  def test_new_initialize_struct_style
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=> :LWES_New,
+                         :name=>:Event1)
+    y = LWES_Foo.new(true, -16, -32, -64, "127.0.0.1", "HI", 16, 32, 64)
+    assert_equal true, y.t_bool
+    assert_equal -16, y.t_int16
+    assert_equal -32, y.t_int32
+    assert_equal -64, y.t_int64
+    assert_equal "127.0.0.1", y.t_ip_addr
+    assert_equal "HI", y.t_string
+    assert_equal 16, y.t_uint16
+    assert_equal 32, y.t_uint32
+    assert_equal 64, y.t_uint64
+  end
+
+  def test_skip_attr
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=> :LWES_Skip,
+                         :name=>:Event1,
+                         :skip => %w(SenderIP SenderPort SiteID ReceiptTime))
+    y = LWES_Skip.new
+    assert ! y.respond_to?(:SenderIP)
+    assert ! y.respond_to?(:SenderPort)
+    assert ! y.respond_to?(:SiteID)
+    assert ! y.respond_to?(:ReceiptTime)
+    assert y.respond_to?(:enc)
+    assert y.respond_to?(:st)
+  end
+
+  def test_skip_regexp
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=> :LWES_SkipRe,
+                         :name=>:Event1,
+                         :skip => %r(\AS))
+    y = LWES_SkipRe.new
+    assert ! y.respond_to?(:SenderIP)
+    assert ! y.respond_to?(:SenderPort)
+    assert ! y.respond_to?(:SiteID)
+    assert y.respond_to?(:ReceiptTime)
+    assert y.respond_to?(:enc)
+    assert y.respond_to?(:st)
+  end
+
+  def test_optional_regexp
+    a = LWES::Struct.new(:file=>ESF_FILE,
+                         :class=> :LWES_OptionalRe,
+                         :name=>:Event1,
+                         :optional=> %r(\AS))
+    expect = {:SenderIP=>true, :SenderPort=>true, :SiteID=>true}
+    assert_equal expect, LWES_OptionalRe::OPTIONAL
   end
 
 end
