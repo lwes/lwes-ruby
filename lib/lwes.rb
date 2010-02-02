9cde7c756715778ee91224e61f478173d57368fa
Erik S. Chang <erik.s.chang@gmail.com>
2010-02-02 01:28:30 +0000

    0.2.3 - hopefully fix OSX issues
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-ruby/trunk@396 a2f82657-cdd2-4550-bd36-68a8e7111808

--- lib/lwes.rb
+++ lib/lwes.rb
@@ -1,6 +1,6 @@
 module LWES
-  # version of our library, currently 0.2.2
-  VERSION = "0.2.2"
+  # version of our library, currently 0.2.3
+  VERSION = "0.2.3"
 
   autoload :TypeDB, "lwes/type_db"
   autoload :Struct, "lwes/struct"
