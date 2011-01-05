123bff4ffec7a43f1d150e90303d4bc8fb6997d4
Erik S. Chang <erik.s.chang@gmail.com>
2011-01-05 23:38:37 +0000

    increment VERSION constant
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-ruby/trunk@632 a2f82657-cdd2-4550-bd36-68a8e7111808

--- lib/lwes.rb
+++ lib/lwes.rb
@@ -1,6 +1,6 @@
 module LWES
-  # version of our library, currently 0.4.0
-  VERSION = "0.4.0"
+  # version of our library, currently 0.6.1
+  VERSION = "0.6.1"
 
   autoload :TypeDB, "lwes/type_db"
   autoload :Struct, "lwes/struct"
